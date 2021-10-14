6ed34b6f3012a9c85ce8de6df41db4cd0324e23f
Patrick Bacaoanu <5bacap25@solent.ac.uk>
2021-10-14 15:09:19 +0100

    test

---
+++ basics/decisions/simple_decisions/if_.py
@@ -0,0 +1,8 @@
+# check whether first number is bigger than the second number.
+# Assume first_number and second_number are existing variables.
+print("What book is this?")
+book = input()
+print()
+print(f"I like reading {book} books!")
+print()
+print("Finished reading book.")
